bc451c5a3246c26e0d2feb0987427408349a8fe6
freef4ll <103502659+freef4ll@users.noreply.github.com>
2022-09-09 09:35:01 +0300

    chore: statistics from stdin and file, making use of the new API [sc-642] (#10)
    chore: read statistics from stdin and file, making use of the new API [sc-642]

--- Sources/LatencyStatisticsCLI/main.swift
+++ Sources/LatencyStatisticsCLI/main.swift
@@ -1,17 +1,50 @@
 import LatencyStatistics
 
+func parseNumbersFromString(_ content: String) -> [Int] {
+    let stringNumbers = content.split { char in
+        if char.isNumber {
+            return false
+        } else {
+            return true
+        }
+    }
+    return stringNumbers.map { Int($0)! }
+}
+
 func main() {
     let argc = CommandLine.argc
     guard argc == 2 else {
-        print("There should be one argument - file name")
+        print("There should be one argument - file name or '-' for reading from stdin")
         return
     }
 
-//    let fileName = CommandLine.arguments[1]
+    do {
+        var statistics = LatencyStatistics()
+        let fileName = CommandLine.arguments[1]
 
-//    var statistics = LatencyStatistics()
-//    statistics.process(fileName: fileName)
-//    statistics.printStatistics();
+        if fileName == "-" {
+            while let content = readLine(strippingNewline: true) {
+                let measurements = parseNumbersFromString(content)
+
+                for input in measurements {
+                    statistics.add(input)
+                }
+            }
+        } else {
+            let content = try String(contentsOfFile: fileName)
+
+            let measurements = parseNumbersFromString(content)
+
+            for input in measurements {
+                statistics.add(input)
+            }
+        }
+
+        print(statistics.output())
+    } catch {
+        print(error)
+        return
+    }
 }
 
 main()
